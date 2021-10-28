bc4b216a34eebcab1634563e29e36778377a2506
Zinovatny Maksym <zamaxzamax@gmail.com>
2021-10-28 15:46:34 +0300

    init Sourse

--- TestThree/ViewController.swift
+++ TestThree/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-  
+  var x: Int = 0
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
